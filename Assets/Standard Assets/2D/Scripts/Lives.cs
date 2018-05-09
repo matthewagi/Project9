74392f68be73866e542336166571dda2d0a1827b
matthewagi <matthewagi@gmail.com>
2018-05-09 23:22:53 +0200

    added bounderies and lives

---
+++ Assets/Standard Assets/2D/Scripts/Lives.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour
+{
+    public static float life;
+
+
+
+
+    void Start()
+    {
+
+        life = 4;
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "bounderies")
+        {
+
+            life--;
+            Debug.Log(life);
+            transform.position = new Vector3(-6, 2, 0);
+
+
+
+        }
+
+
+    }
+}
