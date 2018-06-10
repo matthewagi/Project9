323e7ebdd75d72c9fb70184039adc7f6c094370a
matthewagi <matthewagi@gmail.com>
2018-06-10 22:20:15 +0200

    added documents and final build

---
+++ MyUnityProject/Assets/exitgame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class exitgame : MonoBehaviour {
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	public void QuitGame() {
+
+
+        Debug.Log("Game is exiting");
+        Application.Quit();
+           
+            //Just to make sure its working
+        }
+
+
+    }
+
---
+++ MyUnityProject/Assets/menuscript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class menuscript : MonoBehaviour {
+
+	public void Changescene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    
+}
